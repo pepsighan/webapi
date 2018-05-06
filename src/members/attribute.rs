8496c45f860deaa98e0fe66f6358900f1911cfde
Sharad Chand <sharad.d.chand@gmail.com>
2018-05-06 12:56:09 +0545

    Full list of values required to define an attribute completely

--- src/members/attribute.rs
+++ src/members/attribute.rs
@@ -1,22 +1,27 @@
 use weedle::{types::Type, interface::AttributeInterfaceMember};
 use types::Types;
 use traits::IsDefined;
+use std::cmp::Ordering;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Attribute {
-    readonly: bool,
-    identifier: String,
-    type_: Type
+    pub is_global: bool,
+    pub interface: String,
+    pub readonly: bool,
+    pub identifier: String,
+    pub type_: Type
 }
 
 impl Attribute {
-    pub fn scrape(from: &AttributeInterfaceMember, types: &Types) -> Option<Attribute> {
+    pub fn scrape(from: &AttributeInterfaceMember, is_global: bool, interface: &str, types: &Types) -> Option<Attribute> {
         if from.type_.type_.is_defined(types) {
             let readonly = from.readonly.is_some();
             let identifier = from.identifier.name.clone();
             let type_ = from.type_.type_.clone();
 
             Some(Attribute {
+                is_global,
+                interface: interface.to_string(),
                 readonly,
                 identifier,
                 type_
